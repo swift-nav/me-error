8065f76f6222de006fff4e8f963997f26311728c
ekalosak <eric.kalosa.kenyon@swift-nav.com>
2019-05-06 13:35:36 -0700

    extracting decoded msg74

--- src/extract_msg74.sh
+++ src/extract_msg74.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cat data/00ec3270-7061-4d3d-a570-47b1eb753e04_PK40ac4c023b714c8aaaf2377242a2b1d3.sbp.json | jq '.data | select(.msg_type==74)' -rc
+cat data/00ec3270-7061-4d3d-a570-47b1eb753e04_PK40ac4c023b714c8aaaf2377242a2b1d3.sbp.json | json2json | jq '.data | select(.msg_type==74)' -rc
 
